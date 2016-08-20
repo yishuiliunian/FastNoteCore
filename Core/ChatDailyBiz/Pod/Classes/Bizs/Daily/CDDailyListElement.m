0956377dfdf29b395373cb27c560f08964458e4b
stonedong <yishuiliunian@gmail.com>
2016-08-20 19:07:35 +0800

    工程跑起来了

---
+++ Core/ChatDailyBiz/Pod/Classes/Bizs/Daily/CDDailyListElement.m
@@ -0,0 +1,37 @@
+//
+//  CDDailyListElement.m
+//  Pods
+//
+//  Created by stonedong on 16/8/20.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "CDDailyListElement.h"
+#import "CDDailyListViewController.h"
+@implementation CDDailyListElement
+- (void) reloadData
+{
+    [super reloadData];
+}
+
+- (void) willBeginHandleResponser:(CDDailyListViewController *)responser
+{
+    [super willBeginHandleResponser:responser];
+}
+
+- (void) didBeginHandleResponser:(CDDailyListViewController *)responser
+{
+    [super didBeginHandleResponser:responser];
+}
+
+- (void) willRegsinHandleResponser:(CDDailyListViewController *)responser
+{
+    [super willRegsinHandleResponser:responser];
+}
+
+- (void) didRegsinHandleResponser:(CDDailyListViewController *)responser
+{
+    [super didRegsinHandleResponser:responser];
+}
+@end
