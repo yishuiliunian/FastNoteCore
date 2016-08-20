7c57930c0b63403d40ea4084969b70831e28dcb2
stonedong <yishuiliunian@gmail.com>
2016-08-20 17:33:43 +0800

    xx

---
+++ Core/ChatDailyBiz/Pod/Classes/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatDaily
+//
+//  Created by stonedong on 16/8/20.
+//  Copyright © 2016年 stonedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
